981e48001b8d061ddfd0f30ec0e8e71b24ce79d7
Evgeni Begelfor <begelfor@gmail.com>
2025-02-23 23:53:46 +0200

    dfsa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// module.exports = {
-  // publicPath: '/memory-game/'
-// }
 export default defineConfig({
   base: '/memory-game/',
   plugins: [vue()]
-}) 
+})
+
+// module.exports = {
+  // publicPath: '/memory-game/'
+// }
+
+// vue.config.js
+
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+// }; 
